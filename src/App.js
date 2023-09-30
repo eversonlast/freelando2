d0a8fc9258fee435d2a329fbff1ff34c25e31608
eversonlast <33987857+eversonlast@users.noreply.github.com>
2023-09-30 18:09:31 -0300

    Inserido o Estilos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Estilos } from './componentes/EstilosGlobais/Estilos';
 
 function App() {
   return (
     <div className="App">
+      <Estilos />
         <h1>Freelando</h1>
     </div>
   );
---
+++ src/componentes/EstilosGlobais/Estilos.jsx
@@ -0,0 +1,15 @@
+import { Global, css } from "@emotion/react"
+
+const estilos = css `
+    *{
+        font-family: 'Montserrat', sans-serif;
+    }
+`
+
+export const Estilos = () =>{
+    return(
+        <Global 
+            styles={estilos}
+        />
+    )
+}
